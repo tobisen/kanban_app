b103f357f4283dc8818438e310f82952830d96d3
Tobisen <tobias.bertilsson@gmail.com>
2016-02-15 15:51:35 +0100

    React installerat

--- webpack.config.js
+++ webpack.config.js
@@ -11,33 +11,45 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-const common = {
 
-  // Entry accepts a path or an object of entries. We'll be using the
-  // latter form given it's convenient with more complex configurations.
+process.env.BABEL_ENV = TARGET;
+
+const common = {
   entry: {
     app: PATHS.app
   },
+
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
   },
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['eslint'],
-        include: PATHS.app
-      }
-    ],
     loaders: [
       {
-        // Test expects a RegExp! Note the slashes!
         test: /\.css$/,
         loaders: ['style', 'css'],
-        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       }
+
     ]
   }
 };
