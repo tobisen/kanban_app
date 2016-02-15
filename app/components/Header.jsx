7078ae0f13f83b542f80cad207ecda16091e81fb
Tobisen <tobias.bertilsson@gmail.com>
2016-02-15 16:44:31 +0100

    Inför steget att göra ändringar i listan

---
+++ app/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => <div>
+  <h1>Learn Webpack</h1>
+  <p>En tutorial om hur man bygger en kanban app med nodejs, react, mm. Källa:
+    <a href='http://survivejs.com/webpack_react/introduction/' target='_blank'>SurviveJS</a>
+  </p>
+  <hr />
+</div>;
